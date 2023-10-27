cc6802713b7369976533ba249274ddbb83dc3325
nikhil9192 <nikhilsinghtech2580@gmail.com>
2023-10-27 15:06:16 +0530

    Add project files.

---
+++ GithubprojectTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GithubprojectTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("It is is my first day project work");
+            Console.ReadLine();
+        }
+    }
+}
